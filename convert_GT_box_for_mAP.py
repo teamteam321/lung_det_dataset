1cc72f6729dc2b46c74d4e5bdac9d94c8a878d7b
teamteam321 <36959658+teamteam321@users.noreply.github.com>
2021-04-22 21:30:03 +0700

    Update convert_GT_box_for_mAP.py

--- convert_GT_box_for_mAP.py
+++ convert_GT_box_for_mAP.py
@@ -1,6 +1,13 @@
 
+import os
+
+
+
 def main():
-    gt = open('C:\\PROJECT_ALL\\SITE\\fold4\\val.csv','r')
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    gt = open(current_path+'/merge_075.csv','r')
+    out_path = current_path+'/out_box/'
+
 
 
     gt_list = []
@@ -18,8 +25,8 @@ def main():
         st = st[st.rindex('/')+1:st.rindex('.')]
         gt_img_name.append(st)
 
-        wr = open('C:\\PROJECT_ALL\\SITE\\mAP-master\\DEBUGLIST.txt','a')
-        wr.write(st+'\n')
+        #wr = open(current_path+'DEBUGLIST.txt','a')
+        #wr.write(st+'\n')
 
         st = r.split(',')[1:5]
         gt_bbox.append(list(map(int,st)))
@@ -28,7 +35,7 @@ def main():
     print(len(set(gt_img_name)))
     print(gt_img_name[1000])
     for i,x in enumerate(zip(gt_img_name,gt_bbox)):
-        wr = open('C:\\PROJECT_ALL\\SITE\\mAP-master\\out_box\\'+x[0]+".txt",'a')
+        wr = open(out_path+x[0][:x[0].rindex("_Nodule")]+".txt",'a')
         wr.write('tumor '+str(x[1][0])+' '+str(x[1][1])+' '+str(x[1][2])+' '+str(x[1][3])+'\n')
     #for x in zip(gt_img_name,gt_bbox):
         #wr = open('C:\\PROJECT_ALL\\SITE\\mAP-master\\DEBUGLIST.txt','a')
